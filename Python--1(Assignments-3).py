d81e530f26fd38ab7c8f5fa2c155476edce5c1c5
Rosme Baby Joseph <43903262+Rosmej@users.noreply.github.com>
2018-10-07 21:46:48 +0530

    Python-1

---
+++ Python--1(Assignments-3).py
@@ -0,0 +1,11 @@
+
+# coding: utf-8
+
+# In[5]:
+
+
+import math
+r=12
+volume = 4/3 * math.pi*math.pow(r,3)
+print("volume of sphere is {0:.2f}".format(volume))
+
---
+++ Python-1(Assignment-2).py
@@ -0,0 +1,10 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+firstname=input("Input your First name:")
+Lastname=input("Input your Last name:")
+print(Lastname+" "+firstname)
+
