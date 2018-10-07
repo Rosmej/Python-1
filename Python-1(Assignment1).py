45c6d94d0cf006853898e89317671be13ea0b68a
Rosme Baby Joseph <43903262+Rosmej@users.noreply.github.com>
2018-10-07 22:37:26 +0530

    Assignment1

---
+++ Python-1(Assignment1).py
@@ -0,0 +1,15 @@
+
+# coding: utf-8
+
+# In[10]:
+
+
+samp_list = [10, 20, 30, 20, 10, 50, 60, 40, 80, 50, 40]
+dup_items = set()
+uniq_items = []
+for x in samp_list:
+    if x not in dup_items:
+        uniq_items.append(x)
+        dup_items.add(x)
+print(dup_items)
+
